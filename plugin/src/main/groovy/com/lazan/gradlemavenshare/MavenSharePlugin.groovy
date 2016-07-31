1e0a6973898f524c602b075f6b0a79f8f2eaeaa0
uklance <uklance@gmail.com>
2016-07-31 20:44:12 +0100

    apply plugin: 'com.lazan.gradlemavenshareroot'

--- plugin/src/main/groovy/com/lazan/gradlemavenshare/MavenSharePlugin.groovy
+++ plugin/src/main/groovy/com/lazan/gradlemavenshare/MavenSharePlugin.groovy
@@ -5,6 +5,6 @@ import org.gradle.api.*
 class MavenSharePlugin implements Plugin<Project> {
 	void apply(Project project) {
 		project.extensions.create('mavenShare', MavenShareModel)
-		project.rootProject.apply(plugin: MavenShareRootPlugin)
+		project.rootProject.apply(plugin: 'com.lazan.gradlemavenshareroot')
 	}
 }
