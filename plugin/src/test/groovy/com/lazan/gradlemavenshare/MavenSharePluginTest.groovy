e62194227331dbe2ada203a3c195354695aaa1a5
uklance <uklance@gmail.com>
2016-08-02 22:17:44 +0100

    testkit test attempt 2

--- plugin/src/test/groovy/com/lazan/gradlemavenshare/MavenSharePluginTest.groovy
+++ plugin/src/test/groovy/com/lazan/gradlemavenshare/MavenSharePluginTest.groovy
@@ -14,12 +14,14 @@ class MavenSharePluginTest extends Specification {
 	File buildFile
 	File pomFile1
 	File settingsFile
+	File gradlePropertiesFile
 	List<File> pluginClasspath
 	String classpathString
 
 	def setup() {
 		settingsFile = testProjectDir.newFile('settings.gradle')
 		buildFile = testProjectDir.newFile('build.gradle')
+		gradlePropertiesFile = testProjectDir.newFile('gradle.properties')
 		testProjectDir.newFolder('project1')
 		pomFile1 = testProjectDir.newFile('project1/pom.xml')
 		
@@ -34,6 +36,12 @@ class MavenSharePluginTest extends Specification {
 			.collect { it.absolutePath.replace('\\', '/') } // escape backslashes in Windows paths
 			.collect { "'$it'" }
 			.join(", ")
+			
+		def testkitGradlePropsResource = getClass().classLoader.findResource("testkit-gradle.properties")
+		if (testkitGradlePropsResource == null) {
+			throw new IllegalStateException("Could not find testkit-gradle.properties")
+		}
+		gradlePropertiesFile.text = testkitGradlePropsResource.text
 	}
 
 	def "Maven dependency is shared with gradle"() {
@@ -53,7 +61,6 @@ class MavenSharePluginTest extends Specification {
         		}
 				apply plugin: 'java'
 				apply plugin: 'com.lazan.gradlemavenshare'
-				//apply plugin: com.lazan.gradlemavenshare.MavenSharePlugin
 			}
         """
 		
